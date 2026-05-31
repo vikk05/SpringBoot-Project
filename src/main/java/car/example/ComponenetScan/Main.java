4b6457c11e7338b051f153db9675a45954158fd0
Vivek Chauhan <vivekchauhan20150232922@gmail.com>
2026-05-31 12:59:08 +0530

    Day 3 Component Scan and Value Annotation

---
+++ src/main/java/car/example/ComponenetScan/Main.java
@@ -0,0 +1,12 @@
+package car.example.ComponenetScan;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("Component.xml");
+        Employee employee =  context.getBean("employee",Employee.class);
+        System.out.println(employee.toString());
+    }
+}
